80a62561a43af133a8a6bd0ff7e8b1c26c09f2ab
Ghostbird <gijsbertth@gmail.com>
2026-02-08 23:00:40 +0100

    Add Comfy-UI

--- set-up.sh
+++ set-up.sh
@@ -5,9 +5,11 @@
 # - nvidia-container-tookit
 
 sudo apt-get install docker-ce-rootless-extras docker-compose-plugin nvidia-container-toolkit yq nvtop
+# Ensure you can access the data even when created by the container user.
+mkdir -p ollama open-webui storage storage-models storage-user
+chown $(id -u):$(id -g) ollama open-webui storage storage-models storage-user
+chmod g+s ollama open-webui storage storage-models storage-user
 echo
 echo "To start:"
 echo "docker compose up -d"
 echo
-echo "On first use it is recommended to download at least one model:"
-echo "docker exec -t ollama ollama pull <MODEL_NAME>"
